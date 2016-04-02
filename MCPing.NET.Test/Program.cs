316bec23bd70677027821578db136ed79350de5c
nao20010128nao <nao20010128@gmail.com>
2016-04-02 17:05:17 +0900

    テストコード

---
+++ MCPing.NET.Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using nao20010128nao.MCPing.PE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCPing.NET.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UnconnectedPing.DoPing("pe.hypixel.net",19132).ServerName);
+            Console.ReadKey();
+        }
+    }
+}
